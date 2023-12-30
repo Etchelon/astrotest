7b6602ca528d5716560b1a58fc1ea85ae2049dc8
Etchelon <andrea.bertoldo.btf@outlook.com>
2023-12-30 16:18:26 +0100

    svelte integration

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let messages: string[];
+
+  let index = 0;
+  $: greeting = messages[index % messages.length];
+  $: console.log({ greeting });
+</script>
+
+<div>
+  <h3>{greeting}! Thank you for visiting!</h3>
+  <button on:click={() => (++index)}> New Greeting </button>
+</div>
+
+<style>
+  h3 {
+    color: black;
+  }
+</style>
